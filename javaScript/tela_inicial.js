5bc1be302557b04eaee235e81ff388f465b2547b
Rafael <38193394+Rafael-Duarte-Silva@users.noreply.github.com>
2023-06-09 09:14:12 -0300

    Add files via upload

--- javaScript/tela_inicial.js
+++ javaScript/tela_inicial.js
@@ -5,6 +5,17 @@ $(document).ready(() => {
     });
 
     $("#btn_opcoes").click(() => {
-        
+        let html = $("<div></div>");
+
+        html.attr("id", "opcao");
+        html.css("position", "fixed");
+        html.css("width", "110px");
+        html.css("height", "110px");
+        html.css("background-color", "red");
+
+        html.css("z-index", "100");
+
+        $("body").append(html);
+
     });
 });
